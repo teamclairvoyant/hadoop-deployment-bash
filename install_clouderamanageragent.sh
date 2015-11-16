e10554ebb848257b09a3c830f0cf88a3b824d893
Michael Arnold <github@razorsedge.org>
2015-11-16 09:29:06 -0700

    Make clouderamanageragent interactive.

--- install_clouderamanageragent.sh
+++ install_clouderamanageragent.sh
@@ -14,8 +14,14 @@
 #
 # Copyright Clairvoyant 2015
 
+SCMHOST=$1
+if [ -z $SCMHOST ]; then
+  echo "ERROR: Missing SCM hostname."
+  exit 1
+fi
 wget -q http://archive.cloudera.com/cm5/redhat/6/x86_64/cm/cloudera-manager.repo -O /etc/yum.repos.d/cloudera-manager.repo
 yum -y -e1 -d1 install oracle-j2sdk1.7 cloudera-manager-agent
+sed -i -e "/server_host/s|=.*|=${SCMHOST}|" /etc/cloudera-scm-agent/config.ini
 service cloudera-scm-agent start
 chkconfig cloudera-scm-agent on
 
