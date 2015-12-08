5551783c634894da6ffbb6ea19885f80f3e50893
Michael Arnold <github@razorsedge.org>
2015-12-08 10:25:42 -0700

    Allow to not install the Embedded database.

--- install_clouderamanagerserver.sh
+++ install_clouderamanagerserver.sh
@@ -14,15 +14,35 @@
 #
 # Copyright Clairvoyant 2015
 
+INSTALLDB=$1
+if [ -z $INSTALLDB ]; then
+  INSTALLDB=yes
+fi
 if rpm -q redhat-lsb-core; then
   OSREL=`lsb_release -rs | awk -F. '{print $1}'`
 else
   OSREL=`rpm -qf /etc/redhat-release --qf="%{VERSION}\n"`
 fi
 wget -q http://archive.cloudera.com/cm5/redhat/${OSREL}/x86_64/cm/cloudera-manager.repo -O /etc/yum.repos.d/cloudera-manager.repo
-yum -y -e1 -d1 install cloudera-manager-server cloudera-manager-server-db-2
-service cloudera-scm-server-db start
-service cloudera-scm-server start
-chkconfig cloudera-scm-server-db on
-chkconfig cloudera-scm-server on
+if [ "$INSTALLDB" = yes ]; then
+  yum -y -e1 -d1 install cloudera-manager-server-db-2
+  service cloudera-scm-server-db start
+  chkconfig cloudera-scm-server-db on
+fi
+yum -y -e1 -d1 install cloudera-manager-server
+if [ "$INSTALLDB" = yes ]; then
+  service cloudera-scm-server start
+  chkconfig cloudera-scm-server on
+else
+  #yum -y -e1 -d1 install mysql-connector-java postgresql-jdbc
+  echo "** Now you must configure the Cloudera Manager server to connect to the external"
+  echo "** database.  Please edit /etc/cloudera-scm-server/db.properties , then run:"
+  echo "** EITHER"
+  echo "yum -y -e1 -d1 install mysql-connector-java"
+  echo "** OR"
+  echo "yum -y -e1 -d1 install postgresql-jdbc"
+  echo "** AND then"
+  echo "service cloudera-scm-server start"
+  echo "chkconfig cloudera-scm-server on"
+fi
 
