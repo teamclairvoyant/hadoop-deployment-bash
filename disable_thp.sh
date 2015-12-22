52d27b54d7797f82c8626e6acad0283cad7cbd0c
Michael Arnold <github@razorsedge.org>
2015-12-22 14:56:23 -0700

    Fix handling of rc.local on RHEL7.

--- disable_thp.sh
+++ disable_thp.sh
@@ -14,7 +14,20 @@
 #
 # Copyright Clairvoyant 2015
 
-echo never >/sys/kernel/mm/transparent_hugepage/defrag
-sed -i '/transparent_hugepage/d' /etc/rc.local
-echo 'echo never >/sys/kernel/mm/transparent_hugepage/defrag' >>/etc/rc.local
+if rpm -q redhat-lsb-core; then
+  OSREL=`lsb_release -rs | awk -F. '{print $1}'`
+else
+  OSREL=`rpm -qf /etc/redhat-release --qf="%{VERSION}\n"`
+fi
+if [ $OSREL == 6 ]; then
+  echo never >/sys/kernel/mm/transparent_hugepage/defrag
+  sed -i '/transparent_hugepage/d' /etc/rc.local
+  echo 'echo never >/sys/kernel/mm/transparent_hugepage/defrag' >>/etc/rc.local
+else
+  # http://www.certdepot.net/rhel7-rc-local-service/
+  sed -i '/transparent_hugepage/d' /etc/rc.d/rc.local
+  echo 'echo never >/sys/kernel/mm/transparent_hugepage/defrag' >>/etc/rc.d/rc.local
+  chmod +x /etc/rc.d/rc.local
+  systemctl start rc-local
+fi
 
