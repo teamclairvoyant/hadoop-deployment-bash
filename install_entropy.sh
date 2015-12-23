da0f6323cfb9f2b98be9f80669105a1ff2365231
Michael Arnold <github@razorsedge.org>
2015-12-23 15:46:55 -0700

    Switch to installing rng-tools instead of haveged.
    This complies with Cloudera's recommendations.

--- install_entropy.sh
+++ install_entropy.sh
@@ -14,8 +14,39 @@
 #
 # Copyright Clairvoyant 2015
 
-yum -y -e1 -d1 install epel-release
-yum -y -e1 -d1 install haveged
-service haveged start
-chkconfig haveged on
+USEHAVEGED=$1
+
+if rpm -q redhat-lsb-core; then
+  OSREL=`lsb_release -rs | awk -F. '{print $1}'`
+else
+  OSREL=`rpm -qf /etc/redhat-release --qf="%{VERSION}\n"`
+fi
+
+if grep -q rdrand /proc/cpuinfo; then
+  RDRAND=true
+else
+  RDRAND=false
+fi
+
+if [ -n "$USEHAVEGED" ]; then
+  yum -y -e1 -d1 install epel-release
+  yum -y -e1 -d1 install haveged
+  service haveged start
+  chkconfig haveged on
+else
+  # https://www.cloudera.com/content/www/en-us/downloads/navigator/encrypt/3-8-0.html
+  # http://www.certdepot.net/rhel7-get-started-random-number-generator/
+  yum -y -d1 -e1 install rng-tools
+  if [ $RDRAND == false ]; then
+    if [ $OSREL == 6 ]; then
+      sed -i -e 's|^EXTRAOPTIONS=.*|EXTRAOPTIONS="-r /dev/urandom"|' /etc/sysconfig/rngd
+    else
+      cp -p /usr/lib/systemd/system/rngd.service /etc/systemd/system/
+      sed -i -e 's|^ExecStart=.*|ExecStart=/sbin/rngd -f -r /dev/urandom|' /etc/systemd/system/rngd.service
+      systemctl daemon-reload
+    fi
+  fi
+  service rngd start
+  chkconfig rngd on
+fi
 
