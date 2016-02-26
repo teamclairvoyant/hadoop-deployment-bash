a6cea12d2528bc2b38c68a644e9fa8b319fc7188
Michael Arnold <github@razorsedge.org>
2016-02-26 14:31:24 -0700

    Move around less in the filesystem.

--- install_jce.sh
+++ install_jce.sh
@@ -14,26 +14,21 @@
 #
 # Copyright Clairvoyant 2015
 
-pushd /tmp
 if rpm -q jdk || test -d /usr/java/jdk1.6.0_*; then
   wget -c --no-cookies --no-check-certificate --header "Cookie: oraclelicense=accept-securebackup-cookie" \
-    http://download.oracle.com/otn-pub/java/jce_policy/6/jce_policy-6.zip -O jce_policy-6.zip
-  unzip jce_policy-6.zip
-  cp -p jce/*.jar /usr/java/jdk1.6.0_31/jre/lib/security/
+    http://download.oracle.com/otn-pub/java/jce_policy/6/jce_policy-6.zip -O /tmp/jce_policy-6.zip
+  unzip -o -j /tmp/jce_policy-6.zip /usr/java/jdk1.6.0_31/jre/lib/security/
 fi
 
 if rpm -q oracle-j2sdk1.7 || test -d /usr/java/jdk1.7.0_*; then
   wget -c --no-cookies --no-check-certificate --header "Cookie: oraclelicense=accept-securebackup-cookie" \
-    http://download.oracle.com/otn-pub/java/jce/7/UnlimitedJCEPolicyJDK7.zip -O jce_policy-7.zip
-  unzip jce_policy-7.zip
-  cp -p UnlimitedJCEPolicy/*.jar /usr/java/jdk1.7.0_67-cloudera/jre/lib/security/
+    http://download.oracle.com/otn-pub/java/jce/7/UnlimitedJCEPolicyJDK7.zip -O /tmp/jce_policy-7.zip
+  unzip -o -j /tmp/jce_policy-7.zip -d /usr/java/jdk1.7.0_67-cloudera/jre/lib/security/
 fi
 
 if rpm -q oracle-j2sdk1.8 || test -d /usr/java/jdk1.8.0_*; then
   wget -c --no-cookies --no-check-certificate --header "Cookie: oraclelicense=accept-securebackup-cookie" \
-    http://download.oracle.com/otn-pub/java/jce/8/jce_policy-8.zip -O jce_policy-8.zip
-  unzip jce_policy-8.zip
-  cp -p UnlimitedJCEPolicyJDK8/*.jar /usr/java/jdk1.8.0_*/jre/lib/security/
+    http://download.oracle.com/otn-pub/java/jce/8/jce_policy-8.zip -O /tmp/jce_policy-8.zip
+  unzip -o -j /tmp/jce_policy-8.zip -d /usr/java/jdk1.8.0_*/jre/lib/security/
 fi
-popd
 
