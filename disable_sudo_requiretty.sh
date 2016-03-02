3fbcc86991789460f9002689a09236895cb50fbc
Michael Arnold <github@razorsedge.org>
2016-03-02 12:38:57 -0700

    Added disable_sudo_requiretty.sh.

---
+++ disable_sudo_requiretty.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Copyright Clairvoyant 2016
+
+SFILE=/etc/sudoers.d/90-cloud-init-users
+TFILE=/etc/sudoers.d/90-cloud-init-users_requiretty
+
+if [ -f $SFILE ]; then
+  _USER=`grep -v ^# $SFILE | sort -u | awk '{print $1}'`
+  if [ -f $TFILE ]; then
+    sed -e '/requiretty/d' -i $TFILE
+  fi
+  for X in $_USER; do
+    echo "Defaults:${X}"'    !requiretty' >>$TFILE
+  done
+  visudo -cf $TFILE || sed -e '/requiretty/d' -i $TFILE
+fi
+
