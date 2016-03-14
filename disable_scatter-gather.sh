b32053d13c49488e5cc6a52e525cf98da73e18e1
Michael Arnold <github@razorsedge.org>
2016-03-14 14:10:43 -0700

    Added disable_scatter-gather.sh.

---
+++ disable_scatter-gather.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Copyright Clairvoyant 2015
+
+if rpm -q redhat-lsb-core; then
+  OSREL=`lsb_release -rs | awk -F. '{print $1}'`
+else
+  OSREL=`rpm -qf /etc/redhat-release --qf="%{VERSION}\n"`
+fi
+# https://bugs.launchpad.net/ubuntu/+source/linux/+bug/1317811
+if [ $OSREL == 6 ]; then
+  /usr/sbin/ethtool -K eth0 sg off
+  sed -i '/ethtool -K eth0 sg off/d' /etc/rc.local
+  echo '/usr/sbin/ethtool -K eth0 sg off' >>/etc/rc.local
+else
+  # http://www.certdepot.net/rhel7-rc-local-service/
+  sed -i '/ethtool -K eth0 sg off/d' /etc/rc.d/rc.local
+  echo '/usr/sbin/ethtool -K eth0 sg off' >>/etc/rc.d/rc.local
+  chmod +x /etc/rc.d/rc.local
+  systemctl start rc-local
+fi
+
