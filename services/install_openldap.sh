e4aa1a86f16d451c21cd08d0e1ce3d9297a705da
Michael Arnold <github@razorsedge.org>
2017-01-26 21:59:58 -0500

    Update install_openldap.sh to take arguments.

--- services/install_openldap.sh
+++ services/install_openldap.sh
@@ -13,16 +13,130 @@
 # limitations under the License.
 #
 # Copyright Clairvoyant 2015
-
-DOMAIN1=com
-DOMAIN2=clairvoyantsoft
-ROOTDN="cn=Manager,dc=${DOMAIN2},dc=${DOMAIN1}"
-ROOTPW=password
+#
+if [ $DEBUG ]; then set -x; fi
+if [ $DEBUG ]; then ECHO=echo; fi
+#
+##### START CONFIG ###################################################
 
 # http://injustfiveminutes.com/2014/10/28/how-to-initialize-openldap-2-4-x-server-with-olc-on-centos-7/
 # http://www.server-world.info/en/note?os=CentOS_7&p=openldap&f=1
-yum -y -e1 -d1 install openldap-servers openldap-clients
-LDAPPASS=`slappasswd -s $ROOTPW`
+_ROOTDN="Manager"
+
+##### STOP CONFIG ####################################################
+PATH=/usr/bin:/usr/sbin:/bin:/sbin:/usr/local/bin
+YUMOPTS="-y -e1 -d1"
+DATE=`date '+%Y%m%d%H%M%S'`
+
+# Function to print the help screen.
+print_help () {
+  echo "Usage:  $1 --domain <dns domain or kerberos realm>"
+  echo "        $1 [-r|--rootdn <LDAP superuser>]"
+  echo "        $1 [-h|--help]"
+  echo "        $1 [-v|--version]"
+  echo "   ex.  $1"
+  exit 1
+}
+
+# Function to check for root priviledges.
+check_root () {
+  if [[ `/usr/bin/id | awk -F= '{print $2}' | awk -F"(" '{print $1}' 2>/dev/null` -ne 0 ]]; then
+    echo "You must have root priviledges to run this program."
+    exit 2
+  fi
+}
+
+# Function to discover basic OS details.
+discover_os () {
+  if command -v lsb_release >/dev/null; then
+    # CentOS, Ubuntu
+    OS=`lsb_release -is`
+    # 7.2.1511, 14.04
+    OSVER=`lsb_release -rs`
+    # 7, 14
+    OSREL=`echo $OSVER | awk -F. '{print $1}'`
+  else
+    if [ -f /etc/redhat-release ]; then
+      if [ -f /etc/centos-release ]; then
+        OS=CentOS
+      else
+        OS=RedHat
+      fi
+      OSVER=`rpm -qf /etc/redhat-release --qf="%{VERSION}.%{RELEASE}\n" | awk -F. '{print $1"."$2}'`
+      OSREL=`rpm -qf /etc/redhat-release --qf="%{VERSION}\n"`
+    fi
+  fi
+}
+
+## If the variable DEBUG is set, then turn on tracing.
+## http://www.research.att.com/lists/ast-users/2003/05/msg00009.html
+#if [ $DEBUG ]; then
+#  # This will turn on the ksh xtrace option for mainline code
+#  set -x
+#
+#  # This will turn on the ksh xtrace option for all functions
+#  typeset +f |
+#  while read F junk
+#  do
+#    typeset -ft $F
+#  done
+#  unset F junk
+#fi
+
+# Process arguments.
+while [[ $1 = -* ]]; do
+  case $1 in
+    -d|--domain)
+      shift
+      _DOMAIN_LOWER=`echo $1 | tr '[:upper:]' '[:lower:]'`
+      ;;
+    -r|--rootdn)
+      shift
+      _ROOTDN="$1"
+      ;;
+    -h|--help)
+      print_help "$(basename $0)"
+      ;;
+    -v|--version)
+      echo "\tScript"
+      echo "\tVersion: $VERSION"
+      echo "\tWritten by: $AUTHOR"
+      exit 0
+      ;;
+    *)
+      print_help "$(basename $0)"
+      ;;
+  esac
+  shift
+done
+
+# Check to see if we are on a supported OS.
+# Currently only EL.
+discover_os
+if [ "$OS" != RedHat -a "$OS" != CentOS ]; then
+  echo "ERROR: Unsupported OS."
+  exit 3
+fi
+
+# Check to see if we have the required parameters.
+if [ -z "$_DOMAIN_LOWER" ]; then print_help "$(basename $0)"; fi
+
+# Lets not bother continuing unless we have the privs to do something.
+check_root
+
+# main
+_SUFFIX=`echo ${_DOMAIN_LOWER} | awk -F. '{print "dc="$1",dc="$2}'`
+_ROOTDN=`echo "$_ROOTDN" | sed -e 's|cn=||' -e "s|,${_SUFFIX}||"`
+_ROOTDN="cn=${_ROOTDN},${_SUFFIX}"
+
+yum $YUMOPTS install openldap-servers openldap-clients
+
+_PASS=`apg -a 1 -M NCL -m 20 -x 20 -n 1`
+if [ -z "$_PASS" ]; then
+  _PASS=`< /dev/urandom tr -dc A-Za-z0-9 | head -c 20;echo`
+fi
+_ROOTPW=${_PASS}
+_LDAPPASS=`slappasswd -s $_ROOTPW`
 cp -p /usr/share/openldap-servers/DB_CONFIG.example /var/lib/ldap/DB_CONFIG
 chown -R ldap:ldap /var/lib/ldap
 restorecon -rv /var/lib/ldap
@@ -38,38 +152,40 @@ ldapmodify -Q -Y EXTERNAL -H ldapi:/// <<EOF
 dn: olcDatabase={2}hdb,cn=config
 changetype: modify
 replace: olcSuffix
-olcSuffix: dc=${DOMAIN2},dc=${DOMAIN1}
+olcSuffix: $_SUFFIX
 EOF
 ldapmodify -Q -Y EXTERNAL -H ldapi:/// <<EOF
 dn: olcDatabase={2}hdb,cn=config
 changetype: modify
 replace: olcRootDN
-olcRootDN: $ROOTDN
+olcRootDN: $_ROOTDN
 EOF
 ldapmodify -Q -Y EXTERNAL -H ldapi:/// <<EOF
 dn: olcDatabase={1}monitor,cn=config
 changetype: modify
 replace: olcAccess
-olcAccess: {0}to * by dn.base="gidNumber=0+uidNumber=0,cn=peercred,cn=external,cn=auth" read by dn.base="$ROOTDN" read by * none
+olcAccess: {0}to * by dn.base="gidNumber=0+uidNumber=0,cn=peercred,cn=external,cn=auth" read by dn.base="${_ROOTDN}" read by * none
 EOF
 ldapmodify -Q -Y EXTERNAL -H ldapi:/// <<EOF
 dn: olcDatabase={2}hdb,cn=config
 changetype: modify
 add: olcAccess
-olcAccess: {0}to attrs=userPassword,shadowLastChange by dn="$ROOTDN" write by anonymous auth by self write by * none
+olcAccess: {0}to attrs=userPassword,shadowLastChange by dn="${_ROOTDN}" write by anonymous auth by self write by * none
 olcAccess: {1}to dn.base="" by * read
-olcAccess: {2}to * by dn="$ROOTDN" write by * read
+olcAccess: {2}to * by dn="${_ROOTDN}" write by * read
 EOF
 ldapmodify -Q -Y EXTERNAL -H ldapi:/// <<EOF
 dn: olcDatabase={2}hdb,cn=config
 changetype: modify
 add: olcRootPW
-olcRootPW: $LDAPPASS
+olcRootPW: $_LDAPPASS
 EOF
 
+echo "${_ROOTDN} : ${_ROOTPW}"
+
+cp -p /etc/openldap/ldap.conf /etc/openldap/ldap.conf.${DATE}
 cat <<EOF >>/etc/openldap/ldap.conf
-BASE            dc=${DOMAIN2},dc=${DOMAIN1}
+BASE            ${_SUFFIX}
 URI             ldap://$(hostname -f)
 EOF
 
-exit 0
