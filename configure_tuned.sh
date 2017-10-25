66343ef0316840dc1f8e609bff159dff1e7d62d3
Michael Arnold <github@razorsedge.org>
2017-10-24 23:35:01 -0400

    Set profile to throughput-performance on CentOS.
    http://jperrin.org/centos/boosting-centos-server-performance/

--- configure_tuned.sh
+++ configure_tuned.sh
@@ -38,6 +38,11 @@ discover_os () {
   fi
 }
 
+is_virtual () {
+  egrep -qi 'VirtualBox|VMware|Parallel|Xen|innotek|QEMU|Virtual Machine' /sys/devices/virtual/dmi/id/*
+  return $?
+}
+
 echo "********************************************************************************"
 echo "*** $(basename $0)"
 echo "********************************************************************************"
@@ -49,7 +54,10 @@ if [ "$OS" != RedHatEnterpriseServer -a "$OS" != CentOS ]; then
   exit 3
 fi
 
-if ! rpm -q tuned; then exit 0; fi
+if ! rpm -q tuned; then
+  echo "tuned not installed. Exiting."
+  exit 0
+fi
 
 if [ "$OSREL" == 6 ]; then
   PROFILE=`tuned-adm active | awk '{print $NF}' | head -1`
@@ -57,6 +65,9 @@ if [ "$OSREL" == 6 ]; then
 fi
 if [ "$OSREL" == 7 ]; then
   PROFILE=`tuned-adm active | awk '{print $NF}'`
+  if [ "$OS" == CentOS ] && [ "$PROFILE" == balanced ] && ! is_virtual; then
+    PROFILE=throughput-performance
+  fi
   mkdir /etc/tuned/${PROFILE}
   sed -e '/^vm.swappiness/s|= .*|= 1|' /usr/lib/tuned/${PROFILE}/tuned.conf >/etc/tuned/${PROFILE}/tuned.conf
   chown root:root /etc/tuned/${PROFILE}/tuned.conf
