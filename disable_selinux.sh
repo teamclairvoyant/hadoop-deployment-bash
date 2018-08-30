b87c942b2928cecc85f8fea6de018aa0dec6f6f7
Michael Arnold <github@razorsedge.org>
2018-08-30 18:19:50 -0400

    Allow disable_selinux.sh to alternately set Permissive.

--- disable_selinux.sh
+++ disable_selinux.sh
@@ -13,6 +13,32 @@
 # limitations under the License.
 #
 # Copyright Clairvoyant 2015
+#
+if [ -n "$DEBUG" ]; then set -x; fi
+#
+##### START CONFIG ###################################################
+
+##### STOP CONFIG ####################################################
+PATH=/usr/bin:/usr/sbin:/bin:/sbin:/usr/local/bin
+PERMISSIVE=no
+
+# Function to print the help screen.
+print_help() {
+  echo "Usage:  $1 [--permissive]"
+  echo "        $1 [-p|--permissive]"
+  echo "        $1 [-h|--help]"
+  echo "        $1 [-v|--version]"
+  echo "   ex.  $1"
+  exit 1
+}
+
+# Function to check for root priviledges.
+check_root() {
+  if [[ $(/usr/bin/id | awk -F= '{print $2}' | awk -F"(" '{print $1}' 2>/dev/null) -ne 0 ]]; then
+    echo "You must have root priviledges to run this program."
+    exit 2
+  fi
+}
 
 # Function to discover basic OS details.
 discover_os() {
@@ -46,6 +72,41 @@ discover_os() {
   fi
 }
 
+## If the variable DEBUG is set, then turn on tracing.
+## http://www.research.att.com/lists/ast-users/2003/05/msg00009.html
+#if [ $DEBUG ]; then
+#  # This will turn on the ksh xtrace option for mainline code
+#  set -x
+#
+#  # This will turn on the ksh xtrace option for all functions
+#  typeset +f |
+#  while read F junk
+#  do
+#    typeset -ft $F
+#  done
+#  unset F junk
+#fi
+
+# Process arguments.
+while [[ $1 = -* ]]; do
+  case $1 in
+    -p|--permissive)
+      PERMISSIVE=yes
+      ;;
+    -h|--help)
+      print_help "$(basename "$0")"
+      ;;
+    -v|--version)
+      echo "Installs an entropy gathering daemon: RNGD ot HAVEGED."
+      exit 0
+      ;;
+    *)
+      print_help "$(basename "$0")"
+      ;;
+  esac
+  shift
+done
+
 echo "********************************************************************************"
 echo "*** $(basename "$0")"
 echo "********************************************************************************"
@@ -56,9 +117,21 @@ if [ "$OS" != RedHatEnterpriseServer ] && [ "$OS" != CentOS ]; then
   exit 3
 fi
 
-echo "Disabling SElinux..."
+# Check to see if we have the required parameters.
+#if [ -z "$PERMISSIVE" ]; then print_help "$(basename "$0")"; fi
+
+# Lets not bother continuing unless we have the privs to do something.
+check_root
+
+# main
 if [ "$OS" == RedHatEnterpriseServer ] || [ "$OS" == CentOS ]; then
+  if [ "$PERMISSIVE" == no ]; then
+    echo "Disabling SElinux..."
+    sed -i -e '/^SELINUX=/s|=.*|=disabled|' /etc/selinux/config
+  else
+    echo "Permissive SElinux..."
+    sed -i -e '/^SELINUX=/s|=.*|=permissive|' /etc/selinux/config
+  fi
   setenforce 0
-  sed -i -e '/^SELINUX=/s|=.*|=disabled|' /etc/selinux/config
 fi
 
