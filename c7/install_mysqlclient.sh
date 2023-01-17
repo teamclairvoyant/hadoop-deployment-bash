a01813de2ef6192e241dd653335fc4b889d8f215
Michael Arnold <github@razorsedge.org>
2023-01-17 14:40:22 -0500

    Add scripts to install Python 3.8 for Hue in CDP 7.1.8.

---
+++ c7/install_mysqlclient.sh
@@ -0,0 +1,145 @@
+#!/bin/bash
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Copyright Clairvoyant 2023
+
+PATH=/usr/bin:/usr/sbin:/bin:/sbin:/usr/local/bin
+
+# Function to discover basic OS details.
+discover_os() {
+  if command -v lsb_release >/dev/null; then
+    # CentOS, Ubuntu, RedHatEnterpriseServer, RedHatEnterprise, Debian, SUSE LINUX, OracleServer
+    # shellcheck disable=SC2034
+    OS=$(lsb_release -is)
+    # CentOS= 6.10, 7.2.1511, Ubuntu= 14.04, RHEL= 6.10, 7.5, SLES= 11, OEL= 7.6
+    # shellcheck disable=SC2034
+    OSVER=$(lsb_release -rs)
+    # 7, 14
+    # shellcheck disable=SC2034
+    OSREL=$(echo "$OSVER" | awk -F. '{print $1}')
+    # Ubuntu= trusty, wheezy, CentOS= Final, RHEL= Santiago, Maipo, SLES= n/a
+    # shellcheck disable=SC2034
+    OSNAME=$(lsb_release -cs)
+  else
+    if [ -f /etc/redhat-release ]; then
+      if [ -f /etc/centos-release ]; then
+        # shellcheck disable=SC2034
+        OS=CentOS
+        # shellcheck disable=SC2034
+        OSREL=$(rpm -qf /etc/centos-release --qf='%{VERSION}\n' | awk -F. '{print $1}')
+        # shellcheck disable=SC2034
+        OSNAME=$(awk -F"[()]" '{print $2}' /etc/centos-release | sed 's| ||g')
+        if [ -z "$OSNAME" ]; then
+          # shellcheck disable=SC2034
+          OSNAME="n/a"
+        fi
+        if [ "$OSREL" -le "6" ]; then
+          # 6.10.el6.centos.12.3
+          # shellcheck disable=SC2034
+          OSVER=$(rpm -qf /etc/centos-release --qf='%{VERSION}.%{RELEASE}\n' | awk -F. '{print $1"."$2}')
+        elif [ "$OSREL" == "7" ]; then
+          # 7.5.1804.4.el7.centos
+          # shellcheck disable=SC2034
+          OSVER=$(rpm -qf /etc/centos-release --qf='%{VERSION}.%{RELEASE}\n' | awk -F. '{print $1"."$2"."$3}')
+        elif [ "$OSREL" == "8" ]; then
+          if [ "$(rpm -qf /etc/centos-release --qf='%{NAME}\n')" == "centos-stream-release" ]; then
+            # shellcheck disable=SC2034
+            OS=CentOSStream
+            # shellcheck disable=SC2034
+            OSVER=$(rpm -qf /etc/centos-release --qf='%{VERSION}\n' | awk -F. '{print $1}')
+          else
+            # shellcheck disable=SC2034
+            OSVER=$(rpm -qf /etc/centos-release --qf='%{VERSION}.%{RELEASE}\n' | awk -F. '{print $1"."$2"."$4}')
+          fi
+        else
+          # shellcheck disable=SC2034
+          OS=CentOSStream
+          # shellcheck disable=SC2034
+          OSVER=$(rpm -qf /etc/centos-release --qf='%{VERSION}\n')
+        fi
+      elif [ -f /etc/oracle-release ]; then
+        # shellcheck disable=SC2034
+        OS=OracleServer
+        # 7.6
+        # shellcheck disable=SC2034
+        OSVER=$(rpm -qf /etc/oracle-release --qf='%{VERSION}\n')
+        # shellcheck disable=SC2034
+        OSNAME="n/a"
+      else
+        # shellcheck disable=SC2034
+        OS=RedHatEnterpriseServer
+        # 8.6, 7.5, 6Server
+        # shellcheck disable=SC2034
+        OSVER=$(rpm -qf /etc/redhat-release --qf='%{VERSION}\n')
+        # shellcheck disable=SC2034
+        OSREL=$(echo "$OSVER" | awk -F. '{print $1}')
+        if [ "$OSVER" == "6Server" ]; then
+          # shellcheck disable=SC2034
+          OSVER=$(rpm -qf /etc/redhat-release --qf='%{RELEASE}\n' | awk -F. '{print $1"."$2}')
+        elif [ "$OSREL" == "8" ]; then
+          # shellcheck disable=SC2034
+          OS=RedHatEnterprise
+        fi
+        # shellcheck disable=SC2034
+        OSNAME=$(awk -F"[()]" '{print $2}' /etc/redhat-release | sed 's| ||g')
+      fi
+      # shellcheck disable=SC2034
+      OSREL=$(echo "$OSVER" | awk -F. '{print $1}')
+    elif [ -f /etc/SuSE-release ]; then
+      if grep -q "^SUSE Linux Enterprise Server" /etc/SuSE-release; then
+        # shellcheck disable=SC2034
+        OS="SUSE LINUX"
+      fi
+      # shellcheck disable=SC2034
+      OSVER=$(rpm -qf /etc/SuSE-release --qf='%{VERSION}\n' | awk -F. '{print $1}')
+      # shellcheck disable=SC2034
+      OSREL=$(echo "$OSVER" | awk -F. '{print $1}')
+      # shellcheck disable=SC2034
+      OSNAME="n/a"
+    fi
+  fi
+}
+
+echo "********************************************************************************"
+echo "*** $(basename "$0")"
+echo "********************************************************************************"
+# Check to see if we are on a supported OS.
+discover_os
+if [ "$OS" != RedHatEnterpriseServer ] && [ "$OS" != CentOS ] && [ "$OS" != OracleServer ] && [ "$OS" != Ubuntu ]; then
+  echo "ERROR: Unsupported OS."
+  exit 3
+fi
+
+echo "Installing MySQL client into Python3..."
+if [ "$OS" == RedHatEnterpriseServer ] || [ "$OS" == CentOS ] || [ "$OS" == OracleServer ]; then
+  if [ "$OSREL" == 7 ]; then
+    if [ ! -f /opt/rh/rh-python38/enable ]; then
+      echo "ERROR: Python 3.8 is not installed. Exiting..."
+      exit 10
+    fi
+    # shellcheck disable=SC1091
+    source /opt/rh/rh-python38/enable
+    yum -y -e1 -d1 install mysql-devel
+  elif [ "$OSREL" == 8 ]; then
+    yum -y -e1 -d1 install mysql-devel
+  fi
+  yum -y -e1 -d1 install xmlsec1 xmlsec1-openssl
+  yum -y -e1 -d1 install gcc openssl-devel bzip2-devel libffi-devel zlib-devel
+  pip3.8 install mysqlclient
+elif [ "$OS" == Ubuntu ]; then
+  export DEBIAN_FRONTEND=noninteractive
+  apt-get -y -q install xmlsec1 libxmlsec1-openssl libmysqlclient-dev
+  pip3.8 install mysqlclient
+fi
+
---
+++ c7/install_python38.sh
@@ -0,0 +1,140 @@
+#!/bin/bash
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Copyright Clairvoyant 2023
+
+PATH=/usr/bin:/usr/sbin:/bin:/sbin:/usr/local/bin
+
+# Function to discover basic OS details.
+discover_os() {
+  if command -v lsb_release >/dev/null; then
+    # CentOS, Ubuntu, RedHatEnterpriseServer, RedHatEnterprise, Debian, SUSE LINUX, OracleServer
+    # shellcheck disable=SC2034
+    OS=$(lsb_release -is)
+    # CentOS= 6.10, 7.2.1511, Ubuntu= 14.04, RHEL= 6.10, 7.5, SLES= 11, OEL= 7.6
+    # shellcheck disable=SC2034
+    OSVER=$(lsb_release -rs)
+    # 7, 14
+    # shellcheck disable=SC2034
+    OSREL=$(echo "$OSVER" | awk -F. '{print $1}')
+    # Ubuntu= trusty, wheezy, CentOS= Final, RHEL= Santiago, Maipo, SLES= n/a
+    # shellcheck disable=SC2034
+    OSNAME=$(lsb_release -cs)
+  else
+    if [ -f /etc/redhat-release ]; then
+      if [ -f /etc/centos-release ]; then
+        # shellcheck disable=SC2034
+        OS=CentOS
+        # shellcheck disable=SC2034
+        OSREL=$(rpm -qf /etc/centos-release --qf='%{VERSION}\n' | awk -F. '{print $1}')
+        # shellcheck disable=SC2034
+        OSNAME=$(awk -F"[()]" '{print $2}' /etc/centos-release | sed 's| ||g')
+        if [ -z "$OSNAME" ]; then
+          # shellcheck disable=SC2034
+          OSNAME="n/a"
+        fi
+        if [ "$OSREL" -le "6" ]; then
+          # 6.10.el6.centos.12.3
+          # shellcheck disable=SC2034
+          OSVER=$(rpm -qf /etc/centos-release --qf='%{VERSION}.%{RELEASE}\n' | awk -F. '{print $1"."$2}')
+        elif [ "$OSREL" == "7" ]; then
+          # 7.5.1804.4.el7.centos
+          # shellcheck disable=SC2034
+          OSVER=$(rpm -qf /etc/centos-release --qf='%{VERSION}.%{RELEASE}\n' | awk -F. '{print $1"."$2"."$3}')
+        elif [ "$OSREL" == "8" ]; then
+          if [ "$(rpm -qf /etc/centos-release --qf='%{NAME}\n')" == "centos-stream-release" ]; then
+            # shellcheck disable=SC2034
+            OS=CentOSStream
+            # shellcheck disable=SC2034
+            OSVER=$(rpm -qf /etc/centos-release --qf='%{VERSION}\n' | awk -F. '{print $1}')
+          else
+            # shellcheck disable=SC2034
+            OSVER=$(rpm -qf /etc/centos-release --qf='%{VERSION}.%{RELEASE}\n' | awk -F. '{print $1"."$2"."$4}')
+          fi
+        else
+          # shellcheck disable=SC2034
+          OS=CentOSStream
+          # shellcheck disable=SC2034
+          OSVER=$(rpm -qf /etc/centos-release --qf='%{VERSION}\n')
+        fi
+      elif [ -f /etc/oracle-release ]; then
+        # shellcheck disable=SC2034
+        OS=OracleServer
+        # 7.6
+        # shellcheck disable=SC2034
+        OSVER=$(rpm -qf /etc/oracle-release --qf='%{VERSION}\n')
+        # shellcheck disable=SC2034
+        OSNAME="n/a"
+      else
+        # shellcheck disable=SC2034
+        OS=RedHatEnterpriseServer
+        # 8.6, 7.5, 6Server
+        # shellcheck disable=SC2034
+        OSVER=$(rpm -qf /etc/redhat-release --qf='%{VERSION}\n')
+        # shellcheck disable=SC2034
+        OSREL=$(echo "$OSVER" | awk -F. '{print $1}')
+        if [ "$OSVER" == "6Server" ]; then
+          # shellcheck disable=SC2034
+          OSVER=$(rpm -qf /etc/redhat-release --qf='%{RELEASE}\n' | awk -F. '{print $1"."$2}')
+        elif [ "$OSREL" == "8" ]; then
+          # shellcheck disable=SC2034
+          OS=RedHatEnterprise
+        fi
+        # shellcheck disable=SC2034
+        OSNAME=$(awk -F"[()]" '{print $2}' /etc/redhat-release | sed 's| ||g')
+      fi
+      # shellcheck disable=SC2034
+      OSREL=$(echo "$OSVER" | awk -F. '{print $1}')
+    elif [ -f /etc/SuSE-release ]; then
+      if grep -q "^SUSE Linux Enterprise Server" /etc/SuSE-release; then
+        # shellcheck disable=SC2034
+        OS="SUSE LINUX"
+      fi
+      # shellcheck disable=SC2034
+      OSVER=$(rpm -qf /etc/SuSE-release --qf='%{VERSION}\n' | awk -F. '{print $1}')
+      # shellcheck disable=SC2034
+      OSREL=$(echo "$OSVER" | awk -F. '{print $1}')
+      # shellcheck disable=SC2034
+      OSNAME="n/a"
+    fi
+  fi
+}
+
+echo "********************************************************************************"
+echo "*** $(basename "$0")"
+echo "********************************************************************************"
+# Check to see if we are on a supported OS.
+discover_os
+if [ "$OS" != RedHatEnterpriseServer ] && [ "$OS" != CentOS ] && [ "$OS" != OracleServer ]; then
+  echo "ERROR: Unsupported OS."
+  exit 3
+fi
+
+echo "Installing Python 3.8..."
+if [ "$OSREL" == 7 ]; then
+  if [ "$OS" == RedHatEnterpriseServer ]; then
+    subscription-manager repos --enable="rhel-server-rhscl-7-rpms"
+  elif [ "$OS" == CentOS ]; then
+    yum -y -e1 -d1 install centos-release-scl
+  elif [ "$OS" == OracleServer ]; then
+    yum -y -e1 -d1 install oracle-softwarecollection-release-el7
+  fi
+  yum -y -e1 -d1 install rh-python38 rh-python38-python-devel
+elif [ "$OSREL" == 8 ]; then
+  yum -y -e1 -d1 install python38 python38-devel python38-pip
+else
+  echo "ERROR: Unsupported OSREL."
+  exit 1
+fi
+
