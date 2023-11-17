733f455285b55933c1b60242528a19351e69b752
Michael Arnold <github@razorsedge.org>
2023-11-17 15:46:20 -0500

    Add tls/copy_cert-{atlas,livy,ranger}.sh.

---
+++ tls/copy_cert-ranger.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Copyright Clairvoyant 2023
+
+PATH=/usr/bin:/usr/sbin:/bin:/sbin:/usr/local/bin
+
+echo "********************************************************************************"
+echo "*** $(basename "$0")"
+echo "********************************************************************************"
+echo "Copying Ranger TLS certs and keys..."
+install -m 0440 -o root -g ranger /opt/cloudera/security/jks/localhost-keystore.jks /opt/cloudera/security/jks/ranger-keystore.jks
+
